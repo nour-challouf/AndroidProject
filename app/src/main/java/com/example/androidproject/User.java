11599182338363f2624e12de03763ae3bc09e78c
Oussema Ouakad <81573320+Godyrex@users.noreply.github.com>
2024-11-12 22:57:57 +0100

    Added database

---
+++ app/src/main/java/com/example/androidproject/User.java
@@ -0,0 +1,72 @@
+package com.example.androidproject;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public String fullName;
+    public String email;
+    public String phone;
+    public String username;
+    public String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String fullName, String email, String phone, String username, String password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.username = username;
+        this.password = password;
+    }
+}
+
